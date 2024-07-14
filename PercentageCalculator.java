2c5075fdc8a0931785c5fb3d26356fa70d8dad7b
RonikChatterjee <ronikchatterjee615@gmail.com>
2024-07-14 18:09:31 +0530

    PercentageCalculator.java Modified!!!

--- PercentageCalculator.java
+++ PercentageCalculator.java
@@ -5,17 +5,18 @@ public class PercentageCalculator {
     public static void main(String[] args) {
         System.out.println("Percentage Calculator");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the marks of 1st subjects: ");
+        System.out.print("Enter the marks of 1st subject: ");
         int sub1 = sc.nextInt();
-        System.out.print("Enter the marks of 2nd subjects: ");
+        System.out.print("Enter the marks of 2nd subject: ");
         int sub2 = sc.nextInt();
-        System.out.print("Enter the marks of 3rd subjects: ");
+        System.out.print("Enter the marks of 3rd subject: ");
         int sub3 = sc.nextInt();
-        System.out.print("Enter the marks of 4th subjects: ");
+        System.out.print("Enter the marks of 4th subject: ");
         int sub4 = sc.nextInt();
-        System.out.print("Enter the marks of 5th subjects: ");
+        System.out.print("Enter the marks of 5th subject: ");
         int sub5 = sc.nextInt();
-        int percentage = (sub1 + sub2 + sub3 + sub4 + sub5) / 5;
+        sc.close();
+        float percentage = (float)(sub1 + sub2 + sub3 + sub4 + sub5) / 5;
         System.out.println("The percentage is: " + percentage);
     }
 }
