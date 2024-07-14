6795af9cd4179ee0f2318a679866d914eda3a243
RonikChatterjee <ronikchatterjee615@gmail.com>
2024-07-14 18:34:55 +0530

    PracticeSet1 added!!!

--- CWH/PracticeSet1.java
+++ CWH/PracticeSet1.java
@@ -1,5 +1,57 @@
 package Java_DSA_Interview_Preparation_Course.CWH;
+//import java.util.Scanner;
 
 public class PracticeSet1 {
-    
+    // Question 1: Write a program to sum three numbers in java.
+    // public static void main(String[] args) {
+    //     int num1 = 10;
+    //     int num2 = 20;
+    //     int num3 = 30;
+    //     int sum = num1 + num2 + num3;
+    //     System.out.println("The sum of three numbers is: " + sum);
+    // }
+
+    // Question 2: Write a program to calculate CGPA using marks of five subjects (out of 100).
+    // public static void main(String[] args) {
+    //     float subject1 = 90;
+    //     float subject2 = 80;
+    //     float subject3 = 70;
+    //     float subject4 = 60;
+    //     float subject5 = 50;
+    //     float totalMarks = 500;
+    //     float obtainedMarks = subject1 + subject2 + subject3 + subject4 + subject5;
+    //     float cgpa = obtainedMarks / totalMarks * 10;
+    //     System.out.println("The CGPA is: " + cgpa);
+    // }
+
+    // Question 3: Write a Java program which asks the user to enter his/her name and greets them with "Hello, <name>".
+    // public static void main(String[] args) {
+    //     Scanner sc = new java.util.Scanner(System.in);
+    //     System.out.print("Enter your name: ");
+    //     String name = sc.nextLine();
+    //     System.out.println("Hello, " + name);
+    //     sc.close();
+    // }
+
+    //Question 4: Write a Java program to convert Kilometers to miles.
+    // public static void main(String[] args) {
+    //     Scanner sc = new java.util.Scanner(System.in);
+    //     System.out.print("Enter the distance in Kilometers: ");
+    //     float km = sc.nextFloat();
+    //     float miles = km * 0.621371f;
+    //     System.out.println("The distance in miles is: " + miles);
+    //     sc.close();
+    // }
+
+    // Question 5: Write a Java program to detect whether a number entered by the user is an integer or not.
+    // public static void main(String[] args) {
+    //     Scanner sc = new java.util.Scanner(System.in);
+    //     System.out.print("Enter a number: ");
+    //     if (sc.hasNextInt()) {
+    //         System.out.println("The entered number is an integer.");
+    //     } else {
+    //         System.out.println("The entered number is not an integer.");
+    //     }
+    //     sc.close();
+    // }
 }
